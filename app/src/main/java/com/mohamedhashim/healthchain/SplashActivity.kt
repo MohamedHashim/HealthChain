0fdb0508e3a5f24007acdaf5e27d774bc29021cd
MohamedHashim <sawmido2000@gmail.com>
2018-05-28 01:39:49 +0200

    Initial commit

---
+++ app/src/main/java/com/mohamedhashim/healthchain/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.mohamedhashim.healthchain
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_main)
+    }
+}
